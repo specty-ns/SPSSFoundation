c1ddb0f69deb7f4c8b5be88470673506a74e5d57
Dhruv Shah <dhruvshah2811ds@gmail.com>
2025-01-31 01:08:14 -0500

    responsive and changes to home page

--- scripts/script.js
+++ scripts/script.js
@@ -1,38 +1,51 @@
-// window.onload = function () {
-//     const track = document.querySelector('.banner_track');
-//     const slides = document.querySelectorAll('.banner_block');
-//     const leftBtn = document.getElementById('left');
-//     const rightBtn = document.getElementById('right');
+window.onload = function () {
+    var menubtn = document.getElementById("menu_btn");
+    var nav = document.getElementById("nav");
 
-//     let currentIndex = 0;
-//     const visibleItems = 3;
-//     const totalSlides = slides.length;
+    menubtn.onclick = function(){
+        menubtn.classList.toggle("change");
+        nav.classList.toggle("show")
+    }
+    var navExtend = document.querySelectorAll(".nav_extend");
 
-//     function updateCarousel() {
-//         const offset = -currentIndex * (slides[0].offsetWidth);
-//         track.style.transform = `translateX(${offset}px)`;
-//     }
+    navExtend.forEach(function (item) {
+        item.addEventListener("click", function (event) {
+            event.preventDefault();
 
-//     function moveNext() {
-//         if (currentIndex + visibleItems < totalSlides) {
-//             currentIndex += visibleItems;
-//         } else {
-//             currentIndex = 0;
-//         }
-//         updateCarousel();
-//     }
+            navExtend.forEach((el) => {
+                if (el !== item) {
+                    el.classList.remove("dropit");
+                }
+            });
 
-//     function movePrev() {
-//         if (currentIndex - visibleItems >= 0) {
-//             currentIndex -= visibleItems;
-//         } else {
-//             currentIndex = totalSlides - visibleItems;
-//         }
-//         updateCarousel();
-//     }
+            item.classList.toggle("dropit");
+        });
+    });
 
-//     rightBtn.addEventListener('click', moveNext);
-//     leftBtn.addEventListener('click', movePrev);
+    var edu_odo = document.querySelector(".odometer1");
+    var health_odo = document.querySelector(".odometer2");
+    var env_odo = document.querySelector(".odometer3");
+    var emp_odo = document.querySelector(".odometer4");
+    var comm_odo = document.querySelector(".odometer5");
 
-//     setInterval(moveNext, 3000);
-// };
+    const odometer = new Odometer({
+        el: edu_odo,
+    })
+    const odometer2 = new Odometer({
+        el: health_odo,
+    })
+    const odometer3 = new Odometer({
+        el: env_odo,
+    })
+    const odometer4 = new Odometer({
+        el: emp_odo,
+    })
+    const odometer5 = new Odometer({
+        el: comm_odo,
+    })
+    edu_odo.innerHTML = 2800;
+    health_odo.innerHTML = 25800;
+    env_odo.innerHTML = 1800;
+    emp_odo.innerHTML = 140;
+    comm_odo.innerHTML = 41000;
+};
